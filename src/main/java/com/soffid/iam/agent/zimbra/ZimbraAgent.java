69e06b869fb7bb354461ef3ba9bbcc36ef3682ee
Gabriel Buades <gbuades@soffid.com>
2015-04-24 13:17:01 +0200

    Fix account alias transformation to mail list

--- src/main/java/com/soffid/iam/agent/zimbra/ZimbraAgent.java
+++ src/main/java/com/soffid/iam/agent/zimbra/ZimbraAgent.java
@@ -1,62 +1,41 @@
 // Copyright (c) 2000 Govern  de les Illes Balears
 package com.soffid.iam.agent.zimbra;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.InetAddress;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.rmi.RemoteException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Calendar;
 import java.util.Collection;
-import java.util.Date;
+import java.util.Collections;
 import java.util.HashSet;
-import java.util.Hashtable;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Properties;
 import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import es.caib.seycon.ng.comu.Dispatcher;
-import es.caib.seycon.ng.comu.DispatcherAccessControl;
-//import es.caib.seycon.InternalErrorException;
-import es.caib.seycon.ng.exception.InternalErrorException;
-import es.caib.seycon.ng.comu.Password;
-import es.caib.seycon.util.TimedOutException;
-import es.caib.seycon.util.TimedProcess;
-import es.caib.seycon.UnknownUserException;
 import es.caib.seycon.ng.comu.Account;
-import es.caib.seycon.ng.comu.ControlAcces;
+import es.caib.seycon.ng.comu.AttributeDirection;
+import es.caib.seycon.ng.comu.AttributeMapping;
 import es.caib.seycon.ng.comu.DadaUsuari;
-import es.caib.seycon.ng.comu.Grup;
 import es.caib.seycon.ng.comu.LlistaCorreu;
+import es.caib.seycon.ng.comu.Password;
 import es.caib.seycon.ng.comu.Rol;
 import es.caib.seycon.ng.comu.RolGrant;
+import es.caib.seycon.ng.comu.SoffidObjectType;
 import es.caib.seycon.ng.comu.UserAccount;
 import es.caib.seycon.ng.comu.Usuari;
-import es.caib.seycon.ng.exception.UnknownRoleException;
-import es.caib.seycon.ng.remote.RemoteServiceLocator;
-import es.caib.seycon.ng.servei.UsuariService;
+//import es.caib.seycon.InternalErrorException;
+import es.caib.seycon.ng.exception.InternalErrorException;
 import es.caib.seycon.ng.sync.agent.Agent;
-import es.caib.seycon.ng.sync.intf.AccessControlMgr;
-import es.caib.seycon.ng.sync.intf.AccessLogMgr;
-import es.caib.seycon.ng.sync.intf.GroupMgr;
-import es.caib.seycon.ng.sync.intf.LogEntry;
+import es.caib.seycon.ng.sync.engine.extobj.AccountExtensibleObject;
+import es.caib.seycon.ng.sync.engine.extobj.ObjectTranslator;
+import es.caib.seycon.ng.sync.engine.extobj.UserExtensibleObject;
+import es.caib.seycon.ng.sync.intf.ExtensibleObject;
+import es.caib.seycon.ng.sync.intf.ExtensibleObjectMapping;
 import es.caib.seycon.ng.sync.intf.MailAliasMgr;
-import es.caib.seycon.ng.sync.intf.ReconcileMgr;
 import es.caib.seycon.ng.sync.intf.ReconcileMgr2;
-import es.caib.seycon.ng.sync.intf.RoleInfo;
-import es.caib.seycon.ng.sync.intf.RoleMgr;
-import es.caib.seycon.ng.sync.intf.UserInfo;
 import es.caib.seycon.ng.sync.intf.UserMgr;
-import es.caib.seycon.db.LogInfoConnection;
+import es.caib.seycon.util.TimedOutException;
+import es.caib.seycon.util.TimedProcess;
 
 /**
  * Agente to manage Zarafa server
@@ -69,6 +48,9 @@ public class ZimbraAgent extends Agent implements UserMgr, MailAliasMgr,
 	/** zarfa-admin program */
 	final static int TIMEOUT = 30000;
 	private String zimbraAdmin;
+	private String zimbraMailbox;
+	boolean createProfile = false;
+	private String fullNameExpression = null;
 
 	/**
 	 * Constructor
@@ -91,6 +73,15 @@ public class ZimbraAgent extends Agent implements UserMgr, MailAliasMgr,
 		if (zimbraAdmin == null || zimbraAdmin.length() == 0) {
 			zimbraAdmin = "/opt/zimbra/zmprov";
 		}
+		zimbraMailbox = getDispatcher().getParam1();
+		if (zimbraMailbox == null || zimbraMailbox.length() == 0) {
+			zimbraMailbox = "/opt/zimbra/zmmailbox";
+		}
+		String s = getDispatcher().getParam2();
+		if (s != null && s.equals("true")) {
+			createProfile = true;
+		}
+		fullNameExpression = getDispatcher().getParam3();
 		log.info("Starting Zimbra Agent {}", getDispatcher().getCodi(), null);
 	}
 
@@ -136,6 +127,10 @@ public class ZimbraAgent extends Agent implements UserMgr, MailAliasMgr,
 			throws java.rmi.RemoteException,
 			es.caib.seycon.ng.exception.InternalErrorException {
 		try {
+			Account acc = new Account ();
+			acc.setName(account);
+			acc.setDescription(usu.getFullName());
+			String fullName = getFullName(acc, usu);
 			// Comprobar si el usuario existe
 			if (existsZimbraUser(account)) {
 				LinkedList<String> args = new LinkedList<String>();
@@ -152,7 +147,7 @@ public class ZimbraAgent extends Agent implements UserMgr, MailAliasMgr,
 						+ (usu.getSegonLlinatge() == null ? "" : " "
 								+ usu.getSegonLlinatge()));
 				args.add("displayName");
-				args.add(usu.getFullName());
+				args.add(fullName);
 				TimedProcess p = new TimedProcess(TIMEOUT);
 				if (p.exec(args.toArray(new String[args.size()])) != 0) {
 					throw new InternalErrorException("Error executing "
@@ -174,7 +169,7 @@ public class ZimbraAgent extends Agent implements UserMgr, MailAliasMgr,
 						+ (usu.getSegonLlinatge() == null ? "" : " "
 								+ usu.getSegonLlinatge()));
 				args.add("displayName");
-				args.add(usu.getFullName());
+				args.add(fullName);
 				TimedProcess p = new TimedProcess(TIMEOUT);
 				if (p.exec(args.toArray(new String[args.size()])) != 0) {
 					throw new InternalErrorException("Error executing "
@@ -272,8 +267,11 @@ public class ZimbraAgent extends Agent implements UserMgr, MailAliasMgr,
 				LinkedList<String> args = new LinkedList<String>();
 				args.add(zimbraAdmin);
 				args.add("-l");
-				args.add("deleteAccount");
+				args.add("modifyAccount");
 				args.add(account);
+				args.add("zimbraAccountStatus");
+				args.add("closed");
+//				args.add("deleteAccount");
 				TimedProcess p = new TimedProcess(TIMEOUT);
 				if (p.exec(args.toArray(new String[args.size()])) != 0) {
 					throw new InternalErrorException("Error executing "
@@ -281,11 +279,11 @@ public class ZimbraAgent extends Agent implements UserMgr, MailAliasMgr,
 				}
 			}
 		} catch (RemoteException e) {
-			throw new InternalErrorException("Error update password", e);
+			throw new InternalErrorException("Error disabling user", e);
 		} catch (IOException e) {
-			throw new InternalErrorException("Error update password", e);
+			throw new InternalErrorException("Error disabling user", e);
 		} catch (TimedOutException e) {
-			throw new InternalErrorException("Error update password", e);
+			throw new InternalErrorException("Error disabling user", e);
 		}
 	}
 
@@ -293,6 +291,10 @@ public class ZimbraAgent extends Agent implements UserMgr, MailAliasMgr,
 			throws RemoteException,
 			es.caib.seycon.ng.exception.InternalErrorException {
 		try {
+			Account acc = new Account ();
+			acc.setName(account);
+			acc.setDescription(descripcio);
+			String fullName = getFullName(acc, null);
 			// Comprobar si el usuario existe
 			if (existsZimbraUser(account)) {
 				LinkedList<String> args = new LinkedList<String>();
@@ -301,7 +303,7 @@ public class ZimbraAgent extends Agent implements UserMgr, MailAliasMgr,
 				args.add("modifyAccount");
 				args.add(account);
 				args.add("displayName");
-				args.add(descripcio);
+				args.add(fullName);
 				TimedProcess p = new TimedProcess(TIMEOUT);
 				if (p.exec(args.toArray(new String[args.size()])) != 0) {
 					throw new InternalErrorException("Error executing "
@@ -313,7 +315,7 @@ public class ZimbraAgent extends Agent implements UserMgr, MailAliasMgr,
 				args.add("createAccount");
 				args.add(account);
 				args.add("displayName");
-				args.add(descripcio);
+				args.add(fullName);
 				TimedProcess p = new TimedProcess(TIMEOUT);
 				if (p.exec(args.toArray(new String[args.size()])) != 0) {
 					throw new InternalErrorException("Error executing "
@@ -450,7 +452,7 @@ public class ZimbraAgent extends Agent implements UserMgr, MailAliasMgr,
 				LinkedList<String> list = new LinkedList<String>();
 				for (String s : p.getOutput().split("\n")) {
 					if (s.startsWith(prefix))
-						list.add(s.substring(prefix.length()));
+						list.add(s.substring(prefix.length()).toLowerCase());
 				}
 				return list;
 			}
@@ -486,14 +488,35 @@ public class ZimbraAgent extends Agent implements UserMgr, MailAliasMgr,
 			throws InternalErrorException {
 		try {
 			String name = llista.getNom() + "@" + llista.getCodiDomini();
+			if (llista.getLlistaExterns() == null)
+				llista.setLlistaExterns("");
+			if (llista.getLlistaLlistes() == null)
+				llista.setLlistaLlistes("");
+			String users = getUsersList(llista);
+			if (users == null)
+				users = "";
+			if (llista.getLlistaExterns().trim().length() == 0 &&
+					llista.getLlistaLlistes().trim().length() == 0 &&
+					users.trim().length() == 0 )
+			{
+				removeListAlias(llista.getNom(), llista.getCodiDomini());
+				return;
+			}
+			
+			boolean singleUser = llista.getLlistaExterns().trim().isEmpty() && 
+					llista.getLlistaLlistes().trim().isEmpty() &&
+					users.equals(llista.getLlistaUsuaris()) &&
+					!users.trim().contains(",");
+			if (singleUser && existsZimbraList(name))
+			{
+				removeListAlias(llista.getNom(), llista.getCodiDomini());
+			}
+			
 			if (! existsZimbraList(name)) {
-				boolean singleUser = llista.getLlistaExterns().trim().isEmpty() && 
-						llista.getLlistaLlistes().trim().isEmpty() && 
-						!llista.getLlistaUsuaris().trim().contains(",");
-				String owner = singleUser ? getUserEmail (llista.getLlistaUsuaris().trim()): null;
+				String owner = singleUser ? getUserEmail (users.trim()): null;
 				
 				String aliasOwner = getAliasOwner(name);
-				if (aliasOwner == null && singleUser && owner != null)
+				if (aliasOwner == null && singleUser && owner != null && ! owner.equals (name))
 				{
 					// Create alias
 					LinkedList<String> args = new LinkedList<String>();
@@ -507,9 +530,14 @@ public class ZimbraAgent extends Agent implements UserMgr, MailAliasMgr,
 						throw new InternalErrorException("Error executing "
 								+ concat(args) + ":\n" + p.getOutput()+p.getError());
 					}
+					if (createProfile)
+					{
+						Usuari u = getServer().getUserInfo(users, null);
+						createAliasProfile(owner, name, u.getFullName());
+					}
 					return;
 				}
-				else if (singleUser && owner != null && aliasOwner.equals (owner))
+				else if (singleUser && owner != null && owner.equals(name))
 				{
 					// Nothing to do
 					return;
@@ -543,14 +571,14 @@ public class ZimbraAgent extends Agent implements UserMgr, MailAliasMgr,
 				}
 
 			}
-			setListAliasMembers(name, llista);
+			setListAliasMembers(name, llista, users);
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new InternalErrorException("Error processing task", e);
 		}
 	}
 
-	private void setListAliasMembers(String name, LlistaCorreu llista)
+	private void setListAliasMembers(String name, LlistaCorreu llista, String users)
 			throws IOException, TimedOutException, InternalErrorException,
 			es.caib.seycon.ng.exception.UnknownUserException {
 		LinkedList<String> args = new LinkedList<String>();
@@ -569,32 +597,32 @@ public class ZimbraAgent extends Agent implements UserMgr, MailAliasMgr,
 		Set<String> newMembers = new HashSet<String>();
 		if (llista.getLlistaExterns().trim().length() > 0 )
 			for (String extern : llista.getLlistaExterns().split("[ ,]+"))
-				newMembers.add(extern);
+				newMembers.add(extern.toLowerCase());
 
 		if (llista.getLlistaLlistes().trim().length() > 0 )
 			for (String extern : llista.getLlistaLlistes().split("[ ,]+"))
-				newMembers.add(extern);
+				newMembers.add(extern.toLowerCase());
 
-		if (llista.getLlistaUsuaris().trim().length() > 0 )
-			for (String user : llista.getLlistaUsuaris().split("[ ,]+")) {
+		if (users.trim().length() > 0 )
+			for (String user :  users.split("[ ,]+")) {
 				Usuari u = getServer().getUserInfo(user, null);
 				if (u != null) {
 					boolean found = false;
 				
 					for (Account acc : getServer().getUserAccounts(u.getId(),
 							getCodi())) {
-						newMembers.add(acc.getName());
+						newMembers.add(acc.getName().toLowerCase());
 						found = true;
 					}
 					if (! found )
 					{
 						if (u.getDominiCorreu() != null)
-							newMembers.add (u.getNomCurt()+"@"+u.getDominiCorreu());
+							newMembers.add (u.getNomCurt()+"@"+u.getDominiCorreu().toLowerCase());
 						else
 						{
 							DadaUsuari email = getServer().getUserData(u.getId(), "EMAIL");
 							if (email != null && email.getValorDada() != null)
-								newMembers.add (email.getValorDada());
+								newMembers.add (email.getValorDada().toLowerCase());
 								
 						}
 					}
@@ -690,5 +718,88 @@ public class ZimbraAgent extends Agent implements UserMgr, MailAliasMgr,
 			throw new InternalErrorException("Error processing task", e);
 		}
 	}
+	
+	private void createAliasProfile (String account, String alias, String description) throws InternalErrorException
+	{
+		try {
+			TimedProcess p = new TimedProcess(TIMEOUT);
+			if (p.exec(new String[] { zimbraMailbox, "-z", "-m", account, "cid", alias, "zimbraPrefFromAddress", alias,
+					"zimbraPrefFromAddressType", "sendAs", "zimbraPrefFromDisplay", description,
+					"zimbraPrefReplyToEnabled", "FALSE", "zimbraPrefWhenInFoldersEnabled", "FALSE",
+					"zimbraPrefWhenSentToAddresses", alias, "zimbraPrefWhenSentToEnabled", "TRUE"}) != 0)
+			{
+				throw new IOException("Error executing " +  zimbraMailbox + " :\n" + p.getOutput()+p.getError());
+			}
+			if (p.exec(new String[] { zimbraAdmin, "mid", account, alias, "zimbraPrefIdentityName", description}) != 0)
+			{
+				throw new IOException("Error executing " + zimbraMailbox + " :\n" + p.getOutput()+p.getError());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new InternalErrorException("Error processing task", e);
+		}
+	}
+	
+	private String getFullName (Account account, Usuari u) throws InternalErrorException
+	{
+		if (fullNameExpression == null || fullNameExpression.trim().length() == 0)
+			return account.getDescription();
+					
+		ExtensibleObject eo = u == null ? new AccountExtensibleObject(account, getServer())
+			: new UserExtensibleObject(account, u, getServer());
+		ExtensibleObjectMapping eom = new ExtensibleObjectMapping();
+		AttributeMapping am = new AttributeMapping();
+		am.setSystemAttribute("fullName");
+		am.setSoffidAttribute(fullNameExpression);
+		am.setDirection(AttributeDirection.OUTPUT);
+		am.setId((long) 0);
+		am.setObjectId((long) 0);
+		eom.setAttributes(new LinkedList<AttributeMapping>());
+		eom.getAttributes().add(am);
+		eom.setSoffidObject(SoffidObjectType.fromString(eo.getObjectType()));
+		eom.setSystemObject(eo.getObjectType());
 
+		ObjectTranslator ot = new ObjectTranslator(getDispatcher(), getServer(), 
+				Collections.singleton(eom));
+		String s =  (String) ot.generateAttribute("fullName", eo, eom);
+		if ( s == null)
+			throw new InternalErrorException("Error evaluating "+fullNameExpression+": null");
+		if (! s.equals (account.getDescription()))
+		{
+			account.setDescription(s);
+			if (u != null)
+			{
+				u.setFullName(s);
+				int f = s.indexOf(' ');
+				if ( f < 0)
+				{
+					u.setNom(s);
+					u.setPrimerLlinatge("");
+					u.setSegonLlinatge("");
+				} else {
+					u.setNom(s.substring(0, f));
+					u.setPrimerLlinatge(s.substring(f+1));
+					u.setSegonLlinatge("");
+				}
+			}
+		}
+		return s;
+	}
+
+	private String getUsersList (LlistaCorreu llista)
+	{
+		try {
+			Method m  = llista.getClass().getMethod("getExplodedUsersList");
+			return (String) m.invoke(llista);
+		} catch (NoSuchMethodException e ) {
+			return llista.getLlistaUsuaris();
+		} catch (IllegalAccessException e) {
+			throw new RuntimeException (e);
+		} catch (IllegalArgumentException e) {
+			throw new RuntimeException (e);
+		} catch (InvocationTargetException e) {
+			throw new RuntimeException (e);
+		}
+			
+	}
 }
